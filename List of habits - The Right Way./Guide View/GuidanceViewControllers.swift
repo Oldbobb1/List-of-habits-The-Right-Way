f42785a53e4267e7054adbf6c0c9fbc0109f399a
Bobbi R <bobbi@MacBook-Air-Bobbi.local>
2024-06-19 16:30:01 +0300

    0.03

--- List of habits - The Right Way./Guide View/GuidanceViewControllers.swift
+++ List of habits - The Right Way./Guide View/GuidanceViewControllers.swift
@@ -7,37 +7,16 @@
 import UIKit
 
 class GuidanceViewControllers: UIViewController {
-    
-    let label = Label.label(text: "Habits", fontSize: 20)
-    let label1 = Label.label(text: "Habits поможет вам завести и поддерживать полезные привычки, позволяя улучшить свою жизнь.", fontSize: 15)
-    let imageView = Image.createImage("guidePrew")
+
     var swipe: SwipeClass?
+    var guide: GuideModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        updateUI(); swipeActions()
-        
-    }
-    
-    private func updateUI() {
-        
-        view.backgroundColor = UIColor.systemBackground
-        view.addSubview(label)
-        view.addSubview(label1)
-        self.view.addSubview(imageView)
-        
-        imageView.layer.cornerRadius = 150
-        
-        Layout.applyView(imageView, view: view, additionalConstraints: { (make) in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
-            make.width.equalTo(300)
-            make.height.equalTo(300)
-        })
-        
-        Layout.applyView(label, view: view, leadingOffset: 10, trailingOffset: -10, bottomOffset: -80 )
-        Layout.applyView(label1, view: view, leadingOffset: 10, trailingOffset: -10, bottomOffset: -20 )
+        guide = GuideModel()
+        guide.updateUI(view: self.view)
+        swipeActions()
     }
     
     private func swipeActions() {
@@ -54,6 +33,7 @@ class GuidanceViewControllers: UIViewController {
     }
     
 }
+
 
 
 
---
+++ List of habits - The Right Way./ModelGuideView/GuideModel.swift
@@ -0,0 +1,35 @@
+//  GuideModel.swift
+
+//  List of habits - The Right Way.
+
+//  Created by Bobbi R. on 19.06.24.
+
+import UIKit
+
+class GuideModel {
+    
+    func updateUI(view:UIView) {
+        
+        view.backgroundColor = UIColor.systemBackground
+        
+        let label = Label.label(text: "Habits", fontSize: 20)
+        let label1 = Label.label(text: "Habits поможет вам завести и поддерживать полезные привычки, позволяя улучшить свою жизнь.", fontSize: 15)
+        let imageView = Image.createImage("guidePrew")
+        
+        view.addSubview(label)
+        view.addSubview(label1)
+        view.addSubview(imageView)
+        
+        imageView.layer.cornerRadius = 150
+        
+        Layout.applyView(imageView, view: view, additionalConstraints: { (make) in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.width.equalTo(300)
+            make.height.equalTo(300)
+        })
+        
+        Layout.applyView(label, view: view, leadingOffset: 10, trailingOffset: -10, bottomOffset: -80 )
+        Layout.applyView(label1, view: view, leadingOffset: 10, trailingOffset: -10, bottomOffset: -20 )
+    }
+}
