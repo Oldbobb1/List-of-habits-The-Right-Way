c514b0a88575e37eaf4fc8f4508eb4b65bb4b60f
Dmitry <128475324+Oldbobb1@users.noreply.github.com>
2024-07-08 14:14:01 +0300

    Update AppDelegate.swift

--- Habitus/Habitus/Aplication/AppDelegate.swift
+++ Habitus/Habitus/Aplication/AppDelegate.swift
@@ -14,8 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
-              Thread.sleep(forTimeInterval: 0.3)
         return true
         
     }
