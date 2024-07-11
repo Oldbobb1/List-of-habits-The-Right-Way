5ec9ac13bd73e07836ea398b2d4d77cbe16a27ea
Bobbi R <bobbi@MacBook-Air-Bobbi.local>
2024-07-11 19:57:31 +0300

    update 4.0.2

--- Habitus/Habitus/?? BackupСopyVC ??/BackupСopyVC.swift
+++ Habitus/Habitus/?? BackupСopyVC ??/BackupСopyVC.swift
@@ -5,7 +5,17 @@
 //  Created by Bobbi R. on 9.07.24.
 
 import UIKit
+import ElementBuilder
 
 class BackupСopyVC: UIViewController {
     
+    let label = Label.label(text: "subscribe", fontSize: 25, weight: .heavy, textColor: .black, textAlignment: .center)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(label)
+        
+        Layout.applyView(label, view: view, topOffset: 20, leadingOffset: 10, trailingOffset: -10)
+    }
 }
